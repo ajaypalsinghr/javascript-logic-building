6c2fbe5e894a348d9b43d51b57e1981a1fe6f324
Ajay first project <ajaypalsinghr350@gmail.com>
2026-05-11 23:15:56 +0530

    add new file

---
+++ removeduplicate.js
@@ -0,0 +1,14 @@
+
+let array=[0,20,10,30,20,20,30]
+let store;
+for(let i=0;i<array.length;i++)
+{
+  for(let j=i+1;j<array.length;j++)
+  {
+    if(array[i]!==array[j])
+    {
+console.log(array[i])
+    }
+  }
+ 
+}
